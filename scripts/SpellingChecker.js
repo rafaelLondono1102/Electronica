ea477f66532560465c6251720137a592575105a2
rafaelLondono1102 <82980703+rafaelLondono1102@users.noreply.github.com>
2022-05-19 14:51:40 -0500

    Se verifica la escritura de los comandos

---
+++ scripts/SpellingChecker.js
@@ -0,0 +1,32 @@
+class SpellChecker {
+  dictCommands = {
+    mov: "mov",
+    add: "add",
+    sub: "sub",
+    mul: "mul",
+    div: "div",
+    and: "and",
+    xor: "xor",
+  };
+
+  spellCheck(commands) {
+    let right = true;
+    commands.forEach((Element) => {
+      let split = Element.split(" ");
+      let command = split[0];
+      let parameters = split[1].split(",");
+      if (!this.dictCommands.hasOwnProperty(command)) {
+        alert(
+          "Error de escritura en el comando " +
+            command +
+            ", verifique la escritura"
+        );
+        right = false;
+      } else if ((parameters.length < 1) | (parameters.length > 2)) {
+        alert("Error en los Parametros, verifique la sintaxis");
+        right = false;
+      }
+    });
+    return right;
+  }
+}
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,6 @@
 //Lectura del archivo
 let commands = document.getElementById("inputfile");
-var commandArray;
+var commandArray = [];
 
 commands.addEventListener("change", function () {
   var fr = new FileReader();
@@ -13,18 +13,30 @@ commands.addEventListener("change", function () {
       }
       //generar la tabla a mano, demas que hay mejores maneras pero la verdad no se :)
       //Imprimir el numero de la fila donde se va a ver el comando
-      th = document.createElement("th")
-      th.setAttribute("scope","row")
-      row = document.createTextNode(i+1)
-      th.appendChild(row) 
-      //Imprimir el comando en la fila
-      text = document.createTextNode(commandArray[i]);
-      tr = document.createElement("tr");
-      tr.appendChild(th)
-      tr.appendChild(text);
-      //se adiciona 
-      tbody.appendChild(tr);
+    }
+    let spell = new SpellChecker();
+    if (spell.spellCheck(commandArray)) {
+      llenarTabla(commandArray);
+    }else{
+      document.location.reload(true)
     }
   };
   console.log(fr.readAsText(this.files[0]));
 });
+
+function llenarTabla(commandArray) {
+  let tbody = document.getElementById("tbody_commands");
+  for (let i = 0; i < commandArray.length; i++) {
+    th = document.createElement("th");
+    th.setAttribute("scope", "row");
+    row = document.createTextNode(i + 1);
+    th.appendChild(row);
+    //Imprimir el comando en la fila
+    text = document.createTextNode(commandArray[i]);
+    tr = document.createElement("tr");
+    tr.appendChild(th);
+    tr.appendChild(text);
+    //se adiciona
+    tbody.appendChild(tr);
+  }
+}
